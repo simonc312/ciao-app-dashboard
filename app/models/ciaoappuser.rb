964c8c362b780615301873fd4ccac8111d545cbf
simonc312 <simon.c312@berkeley.edu>
2015-04-06 01:39:30 -0700

    fix misspelling cache-counter to counter-cache

--- app/models/ciaoappuser.rb
+++ app/models/ciaoappuser.rb
@@ -1,4 +1,4 @@
 class CiaoAppUser < ActiveRecord::Base
-  belongs_to :partner, inverse_of: :ciaoappuser, cache_counter: :count_of_ciaoappusers
+  belongs_to :partner, inverse_of: :ciaoappuser, counter_cache: :count_of_ciaoappusers
 
 end
