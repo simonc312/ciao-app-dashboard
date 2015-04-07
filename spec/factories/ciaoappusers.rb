78ee54acdae8179d8990f58ff49e8c9e8263bf38
simonc312 <simon.c312@berkeley.edu>
2015-04-07 04:46:32 -0700

    added backets around faker factorygirl usages to give different random results and fixed the misspelled google jsapi so charts display

--- spec/factories/ciaoappusers.rb
+++ spec/factories/ciaoappusers.rb
@@ -2,11 +2,11 @@ require 'faker'
 
 FactoryGirl.define do
   factory :ciaoappuser do
-  	state_id Random.rand(3)
-  	country_code Random.rand(4)
-  	gender [0,1].sample
-    is_active [0,1].sample
-    modified_at Faker::Time.between(2.months.ago, Time.now, :all)
-    signed_up_at Faker::Time.backward(90, :all)
+  	state_id {Random.rand(3)}
+  	country_code {Random.rand(4)}
+  	gender {[0,1].sample}
+    is_active {[0,1].sample}
+    modified_at {Faker::Time.between(7.weeks.ago, Time.now, :all)}
+    signed_up_at {Faker::Time.between(3.months.ago,2.months.ago, :all)}
   end
 end
--- spec/factories/partners.rb
+++ spec/factories/partners.rb
@@ -2,11 +2,11 @@ require 'faker'
 
 FactoryGirl.define do
   factory :partner do
-  	employee_size Random.rand(2000)
-  	revenue_size Random.rand(2000)
-  	address Faker::Address.street_address
-  	city Random.rand(3)
-  	country Random.rand(3)
-  	zipcode Faker::Address.zip_code
+  	employee_size {Random.rand(2000)}
+  	revenue_size {Random.rand(2000)}
+  	address {Faker::Address.street_address}
+  	city {Random.rand(3)}
+  	country {Random.rand(3)}
+  	zipcode {Faker::Address.zip_code}
   end
 end
